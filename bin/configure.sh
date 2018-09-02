2f407d20eaa261cdeaabab2dbf33c13be57e6263
Lukas Fittl <lukas@fittl.com>
2018-09-01 17:26:29 -0700

    WIP

---
+++ bin/configure.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export PATH=~/vendor/awscli/bin:\$PATH
+
+mkdir ~/.aws
+
+cat >> ~/.aws/credentials << EOF
+[default]
+aws_access_key_id = $AWS_KEY
+aws_secret_access_key = $AWS_SECRET_KEY
+EOF
+
+cat >> ~/.aws/config << EOF
+[default]
+region = $AWS_REGION
+EOF
